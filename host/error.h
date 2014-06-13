e6e76d76243023574c0ed621803c2665d7b9f54b
Martin Paljak <martin@martinpaljak.net>
2014-06-13 15:50:09 +0300

    Remove BSD license marking from file headers as well.

--- host/error.h
+++ host/error.h
@@ -1,7 +1,7 @@
 /* Chrome Linux plugin
 *
 * This software is released under either the GNU Library General Public
-* License (see LICENSE.LGPL) or the BSD License (see LICENSE.BSD).
+* License (see LICENSE.LGPL).
 *
 * Note that the only valid version of the LGPL license as far as this
 * project is concerned is the original GNU Library General Public License
