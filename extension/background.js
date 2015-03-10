72cb722a5a813c797c3d7066833d2b295c98181b
Martin Paljak <martin@martinpaljak.net>
2015-03-10 14:25:29 +0200

    Have one longrunning port per origin

--- extension/background.js
+++ extension/background.js
@@ -7,9 +7,18 @@
  * project is concerned is the original GNU Library General Public License
  * Version 2.1, February 1999
  */
+var NO_NATIVE_URL = "https://pubkey.ee/sign/no_native_installed.html";
+var HELLO_URL = "https://pubkey.ee/sign/hello.html";
+var NATIVE_HOST = "ee.ria.esteid";
+var K_SRC = "src";
+var K_ORIGIN = "origin";
+var K_NONCE = "nonce";
+var K_RESULT = "result";
 // Store the nonce/tabID pairs
 var sessions = {};
-var port = null;
+// Stores the longrunning ports per origin
+var ports = {};
+
 console.log("Background page activated");
 // When extension is loaded
 chrome.runtime.onSuspend.addListener(function() {
@@ -23,6 +32,11 @@ chrome.runtime.onSuspend.addListener(function() {
 chrome.runtime.onConnect.addListener(function(port) {
 	console.log("Connection from " + port.name);
 });
+// When extension is installed
+chrome.runtime.onInstalled.addListener(function(details) {
+	// Check if native is available or direct to help page.
+	chrome.tabs.create({'url': HELLO_URL + "?" + details.reason});
+});
 // When message is received from page
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	console.log("Marshal from url " + sender.url + " and extension id  " + sender.id);
@@ -34,7 +48,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	}
 	// TODO: Check if the URL is in allowed list or not
 	// Either way forward to native currently
-	sessions[request['nonce']] = sender.tab.id;
+	sessions[request[K_NONCE]] = sender.tab.id;
 	send(request);
 });
 // Send the message back to the originating tab
@@ -42,9 +56,9 @@ function sendResponse(msg) {
 	if(msg) {
 		console.log("RECV: " + JSON.stringify(msg));
 		// Add this
-		msg["src"] = "background.js";
+		msg[K_SRC] = "background.js";
 		// Look up the nonce and send to the same tab
-		var nonce = msg['nonce'];
+		var nonce = msg[K_NONCE];
 		if (nonce) {
 			chrome.tabs.sendMessage(sessions[nonce], msg, function(response) {});
 		} else {
@@ -63,21 +77,20 @@ function send(message) {
 	var stateless = true;
 	console.log("SEND: " + JSON.stringify(message));
 	console.log("SEND is " + stateless ? "stateless" : "longrunning");
-	var hostName = "ee.ria.esteid";
 	if(stateless) {
-		chrome.runtime.sendNativeMessage(hostName, message, function(response) {
-			// in case of error we have the originating message at hand
+		chrome.runtime.sendNativeMessage(NATIVE_HOST, message, function(response) {
+			// in case of error we have the originating message at hand for nonce
 			if (!response) {
 				console.log("ERROR: " + JSON.stringify(chrome.runtime.lastError));
 				var resp = {};
-				resp["src"] = "background.js";
-				resp["nonce"] = message['nonce'];
+				resp[K_SRC] = "background.js";
+				resp[K_NONCE] = message[K_NONCE];
 				// XXX: not really nice but seems the only way to detect other errors
-				var expected_message = "Specified native messaging host not found."
-				if (chrome.runtime.lastError && chrome.runtime.lastError.message === expected_message) {
-					resp["result"] = "no_implementation";
+				var EXPECTED_MESSAGE = "Specified native messaging host not found."
+				if (chrome.runtime.lastError && chrome.runtime.lastError.message === EXPECTED_MESSAGE) {
+					resp[K_RESULT] = "no_implementation";
 				} else {
-					resp["result"] = "technical_error";
+					resp[K_RESULT] = "technical_error";
 				}
 				// TODO: Chrome 41+ allows to send to a specific frame.
 				chrome.tabs.sendMessage(sessions[resp["nonce"]], resp);
@@ -86,12 +99,13 @@ function send(message) {
 			}
 		});
 	} else {
-		if(!port) {
-			console.log("Connecting to native messaging host " + hostName);
-			port = chrome.runtime.connectNative(hostName);
+		if(!ports[message[K_ORIGIN]]) {
+			console.log("Connecting to native messaging host " + NATIVE_HOST);
+			var port = chrome.runtime.connectNative(NATIVE_HOST);
 			port.onMessage.addListener(sendResponse);
 			port.onDisconnect.addListener(onDisconnected);
+			ports[message[K_ORIGIN]] = port;
 		}
-		port.postMessage(message);
+		ports[message[K_ORIGIN]].postMessage(message);
 	}
 }
