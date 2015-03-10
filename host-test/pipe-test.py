2a3830874426211da6329193adec55cd888b3166
Martin Paljak <martin@martinpaljak.net>
2015-03-10 16:08:42 +0200

    Windows default path for build result

--- host-test/pipe-test.py
+++ host-test/pipe-test.py
@@ -12,6 +12,8 @@ def get_exe():
         return "host-osx/build/Release/chrome-token-signing.app/Contents/MacOS/chrome-token-signing"
     elif sys.platform == "linux2":
         return "host-linux/out/chrome-token-signing"
+    elif sys.platform == 'win32':
+        return "host-windows\\Debug\\host-windows.exe"
     else:
         print("Unsupported platform: %s" % sys.platform)
         sys.exit(1)
