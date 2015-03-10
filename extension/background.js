3c52dc56f624c57bcdd39737cfac12b5ad5f5e02
Martin Paljak <martin@martinpaljak.net>
2015-03-10 11:35:54 +0200

    Better detection of missing native component

--- extension/background.js
+++ extension/background.js
@@ -13,66 +13,85 @@ var port = null;
 console.log("Background page activated");
 // When extension is loaded
 chrome.runtime.onSuspend.addListener(function() {
-    console.log("Suspending");
-    // Disconnect the port if necessary
-    if(port) {
-        port.disconnect();
-    }
+	console.log("Suspending");
+	// Disconnect the port if necessary
+	if(port) {
+		port.disconnect();
+	}
 });
 // When extension is loaded
 chrome.runtime.onConnect.addListener(function(port) {
-    console.log("Connection from " + port.name);
+	console.log("Connection from " + port.name);
 });
 // When message is received from page
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log("Marshal from url " + sender.url + " and extension id  " + sender.id);
-    // Check that the sender is extension
-    if(sender.id !== chrome.runtime.id) {
-        console.log('Ignoring message not from our extension');
-        // Not our extension, do nothing
-        return;
-    }
-    // TODO: Check if the URL is in allowed list or not
-    // Either way forward to native currently
-    sessions[request['nonce']] = sender.tab.id;
-    send(request);
+	console.log("Marshal from url " + sender.url + " and extension id  " + sender.id);
+	// Check that the sender is extension
+	if(sender.id !== chrome.runtime.id) {
+		console.log('Ignoring message not from our extension');
+		// Not our extension, do nothing
+		return;
+	}
+	// TODO: Check if the URL is in allowed list or not
+	// Either way forward to native currently
+	sessions[request['nonce']] = sender.tab.id;
+	send(request);
 });
 // Send the message back to the originating tab
 function sendResponse(msg) {
-    if(msg) {
-        console.log("RECV: " + JSON.stringify(msg));
-        // Add this
-        msg["src"] = "background.js";
-        // Look up the nonce and send to the same tab
-        var nonce = msg['nonce'];
-        if (nonce) {
-            chrome.tabs.sendMessage(sessions[nonce], msg, function(response) {});
-        } else {
-            console.log("No nonce in response!");
-        }
-    } else {
-        console.log("ERROR: " + JSON.stringify(chrome.runtime.lastError));
-    }
+	if(msg) {
+		console.log("RECV: " + JSON.stringify(msg));
+		// Add this
+		msg["src"] = "background.js";
+		// Look up the nonce and send to the same tab
+		var nonce = msg['nonce'];
+		if (nonce) {
+			chrome.tabs.sendMessage(sessions[nonce], msg, function(response) {});
+		} else {
+			console.log("No nonce in response!");
+		}
+	} else {
+		console.log("ERROR: " + JSON.stringify(chrome.runtime.lastError));
+	}
 }
 
 function onDisconnected() {
-    console.log("Connection closed: " + chrome.runtime.lastError.message);
+	console.log("Connection closed: " + chrome.runtime.lastError.message);
 }
 
 function send(message) {
-    var stateless = true;
-    console.log("SEND: " + JSON.stringify(message));
-    console.log("SEND is " + stateless ? "stateless" : "longrunning");
-    var hostName = "ee.ria.esteid";
-    if(stateless) {
-        chrome.runtime.sendNativeMessage(hostName, message, sendResponse);
-    } else {
-        if(!port) {
-            console.log("Connecting to native messaging host " + hostName);
-            port = chrome.runtime.connectNative(hostName);
-            port.onMessage.addListener(sendResponse);
-            port.onDisconnect.addListener(onDisconnected);
-        }
-        port.postMessage(message);
-    }
+	var stateless = true;
+	console.log("SEND: " + JSON.stringify(message));
+	console.log("SEND is " + stateless ? "stateless" : "longrunning");
+	var hostName = "ee.ria.esteid";
+	if(stateless) {
+		chrome.runtime.sendNativeMessage(hostName, message, function(response) {
+			// in case of error we have the originating message at hand
+			if (!response) {
+				console.log("ERROR: " + JSON.stringify(chrome.runtime.lastError));
+				var resp = {};
+				resp["src"] = "background.js";
+				resp["nonce"] = message['nonce'];
+				// XXX: not really nice but seems the only way to detect other errors
+				var expected_message = "Specified native messaging host not found."
+				if (chrome.runtime.lastError && chrome.runtime.lastError.message === expected_message) {
+					resp["result"] = "no_implementation";
+				} else {
+					resp["result"] = "technical_error";
+				}
+				// TODO: Chrome 41+ allows to send to a specific frame.
+				chrome.tabs.sendMessage(sessions[resp["nonce"]], resp);
+			} else {
+				sendResponse(response);
+			}
+		});
+	} else {
+		if(!port) {
+			console.log("Connecting to native messaging host " + hostName);
+			port = chrome.runtime.connectNative(hostName);
+			port.onMessage.addListener(sendResponse);
+			port.onDisconnect.addListener(onDisconnected);
+		}
+		port.postMessage(message);
+	}
 }
