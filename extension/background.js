3d9f8475596c2c20c0256c9a2c59125ca2def5b2
Martin Paljak <martin@martinpaljak.net>
2015-03-12 11:39:20 +0200

    Background page now exclusively works with longrunning native hosts

--- extension/background.js
+++ extension/background.js
@@ -19,142 +19,154 @@ var K_NONCE = "nonce";
 var K_RESULT = "result";
 var K_TAB = "tab";
 
-// Store the nonce/tabID pairs
-// Used for routing messages back to the right tab
-var sessions = {};
-
-// Stores the longrunning ports per origin
-// Used to route all request from an origin to the same host instance
+// Stores the longrunning ports per tab
+// Used to route all request from a tab to the same host instance
 var ports = {};
 
+// Probed to false if host component is OK.
+var missing = true;
+
 console.log("Background page activated");
-// When extension is loaded
-chrome.runtime.onSuspend.addListener(function() {
-	console.log("Suspending");
-});
-// When extension is loaded
-chrome.runtime.onConnect.addListener(function(port) {
-	console.log("Connection from " + port.name);
+
+// XXX: probe test, because connectNative() does not allow to check the presence
+// of native component for some reason
+_testNativeComponent().then(function(result) {
+	if (result === "ok")
+		missing = false;
 });
 
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-	console.log("Closing tab " + tabId);
-	for (var k in ports) {
-		// FIXME: ===
-		if (k == tabId) {
-			console.log("Our tab! Closing port");
-			ports[k].disconnect();
-			delete ports[k];
-		}
+// Force kill of native process
+// Becasue Port.disconnect() does not work
+function _killPort(tab) {
+	if (tab in ports) {
+		console.log("KILL " + tab);
+		// Force killing with an empty message
+		ports[tab].postMessage({});
 	}
-});
-// When extension is installed
-chrome.runtime.onInstalled.addListener(function(details) {
-	// Check if native is available or direct to help page.
-	if (details.reason === "install" || details.reason === "update") {
-		// Check during every install or upgrade if we have the native components at hand.
-		// Probe with empty message and expect a "result: invalid_argument" response
+}
+
+// Check if native implementation is OK resolves with "ok", "missing" or "forbidden"
+function _testNativeComponent() {
+	return new Promise(function(resolve, reject) {
 		chrome.runtime.sendNativeMessage(NATIVE_HOST, {}, function(response) {
-			console.log("Query for native component");
 			if (!response) {
-				console.log("ERROR: " + JSON.stringify(chrome.runtime.lastError));
+				console.log("TEST: ERROR " + JSON.stringify(chrome.runtime.lastError));
+				// Try to be smart and do some string matching
 				var permissions = "Access to the specified native messaging host is forbidden.";
+				var missing = "Specified native messaging host not found.";
 				if (chrome.runtime.lastError.message === permissions) {
-					chrome.tabs.create({'url': DEVELOPER_URL + "?" + details.reason});
+					resolve("forbidden")
+				} else if (chrome.runtime.lastError.message === missing) {
+					resolve("missing");
 				} else {
-					chrome.tabs.create({'url': NO_NATIVE_URL + "?" + details.reason});
+					resolve("missing");
 				}
 			} else {
-				console.log("RECV: " + JSON.stringify(response));
-				// TODO: Check for native component version and require upgrade if too old
-				if (details.reason == "install" && response.result == "invalid_argument") {
-					chrome.tabs.create({'url': HELLO_URL + "?" + details.reason});
+				console.log("TEST: " + JSON.stringify(response));
+				if (response["result"] === "invalid_argument") {
+					resolve("ok");
+				} else {
+					resolve("missing"); // TODO: something better here
 				}
 			}
 		});
+	});
+}
+
+
+// When extension is installed, check for native component or direct to helping page
+chrome.runtime.onInstalled.addListener(function(details) {
+	if (details.reason === "install" || details.reason === "update") {
+		_testNativeComponent().then(function(result) {
+				var url = null;
+				if (result === "ok" && details.reason === "install") {
+					url = HELLO_URL;
+				} else if (result === "forbidden") {
+					url = DEVELOPER_URL;
+				} else if (result === "missing"){
+					url = NO_NATIVE_URL;
+				}
+				if (url) {
+					chrome.tabs.create({'url': url + "?" + details.reason});
+				}
+		});
 	}
 });
-// When message is received from page
+
+// When message is received from page send it to native
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	console.log("Marshal from url " + sender.url + " and extension id  " + sender.id);
-	// Check that the sender is extension
 	if(sender.id !== chrome.runtime.id) {
 		console.log('WARNING: Ignoring message not from our extension');
 		// Not our extension, do nothing
 		return;
 	}
 	if (sender.tab) {
-		console.log("Sending tab: " + sender.tab.id);
-		// Check if page is DONE
+		// Check if page is DONE and close the native component without doing anything else
 		if (request["type"] === "DONE") {
-			console.log("Tab is done: " + sender.tab.id);
-			ports[sender.tab.id].disconnect();
-			delete ports[sender.tab.id];
-			return;
+			console.log("DONE " + sender.tab.id);
+			if (sender.tab.id in ports) {
+				// FIXME: would want to use Port.disconnect() here
+				_killPort(sender.tab.id);
+			} 
+		} else {
+			request[K_TAB] = sender.tab.id;
+			if (missing)
+				return _fail_with(request, "no_implementation");
+			// TODO: Check if the URL is in allowed list or not
+			// Either way forward to native currently
+			_forward(request);
 		}
-		// TODO: Check if the URL is in allowed list or not
-		// Either way forward to native currently
-		sessions[request[K_NONCE]] = sender.tab.id;
-		request[K_TAB] = sender.tab.id;
-		send(request);
 	}
 });
+
 // Send the message back to the originating tab
-function sendResponse(msg) {
-	if(msg) {
-		console.log("RECV: " + JSON.stringify(msg));
-		// Add this
-		msg[K_SRC] = "background.js";
-		// Look up the nonce and send to the same tab
-		var nonce = msg[K_NONCE];
-		if (nonce) {
-			chrome.tabs.sendMessage(sessions[nonce], msg, function(response) {});
-		} else {
-			console.log("No nonce in response!");
+function _reply(tab, msg) {
+	msg[K_SRC] = "background.js";
+	chrome.tabs.sendMessage(tab, msg);
+}
+
+// Fail an incoming message if the underlying implementation is not
+// present
+function _fail_with(msg, result) {
+	var resp = {};
+	resp[K_NONCE] = msg[K_NONCE];
+	resp[K_RESULT] = result;
+	_reply(msg[K_TAB], resp);
+}
+
+// Forward a message to the native component
+function _forward(message) {
+	var tabid = message[K_TAB];
+	console.log("SEND " + tabid + ": " + JSON.stringify(message));
+	// Open a port if necessary
+	if(!ports[tabid]) {
+		// For some reason there does not seem to be a way to detect missing components from longrunning ports
+		// So we probe before opening a new port.
+		console.log("OPEN " + tabid + ": " + NATIVE_HOST);
+		// create a new port
+		var port = chrome.runtime.connectNative(NATIVE_HOST);
+		// XXX: does not indicate anything for some reason.
+		if (!port) {
+			console.log("OPEN ERROR: " + JSON.stringify(chrome.runtime.lastError));
 		}
-	} else {
-		console.log("ERROR: " + JSON.stringify(chrome.runtime.lastError));
-	}
-}
-
-function onDisconnected() {
-	console.log("Connection closed: " + chrome.runtime.lastError.message);
-}
-
-function send(message) {
-	var stateless = false;
-	console.log("SEND: " + JSON.stringify(message));
-	console.log("SEND is " + (stateless ? "stateless" : "longrunning"));
-	if(stateless) {
-		chrome.runtime.sendNativeMessage(NATIVE_HOST, message, function(response) {
-			// in case of error we have the originating message at hand for nonce
-			if (!response) {
-				console.log("ERROR: " + JSON.stringify(chrome.runtime.lastError));
-				var resp = {};
-				resp[K_SRC] = "background.js";
-				resp[K_NONCE] = message[K_NONCE];
-				// XXX: not really nice but seems the only way to detect other errors
-				var EXPECTED_MESSAGE = "Specified native messaging host not found."
-				if (chrome.runtime.lastError && chrome.runtime.lastError.message === EXPECTED_MESSAGE) {
-					resp[K_RESULT] = "no_implementation";
-				} else {
-					resp[K_RESULT] = "technical_error";
-				}
-				// TODO: Chrome 41+ allows to send to a specific frame.
-				chrome.tabs.sendMessage(sessions[resp["nonce"]], resp);
+		port.onMessage.addListener(function(response) {
+			if (response) {
+				console.log("RECV "+tabid+": " + JSON.stringify(response));
+				_reply(tabid, response);
 			} else {
-				sendResponse(response);
+				console.log("ERROR "+tabid+": " + JSON.stringify(chrome.runtime.lastError));
+				_fail_with(message, "technical_error");
 			}
 		});
+		port.onDisconnect.addListener(function() {
+			console.log("QUIT " + tabid);
+			delete ports[tabid];
+			// TODO: reject all pending promises for tab, if any
+		});
+		ports[tabid] = port;
+		ports[tabid].postMessage(message);
 	} else {
-		if(!ports[message[K_TAB]]) {
-			console.log("Opening native host for tab " + message[K_TAB]);
-			console.log("Connecting to native messaging host " + NATIVE_HOST);
-			var port = chrome.runtime.connectNative(NATIVE_HOST);
-			port.onMessage.addListener(sendResponse);
-			port.onDisconnect.addListener(onDisconnected);
-			ports[message[K_TAB]] = port;
-		}
-		ports[message[K_TAB]].postMessage(message);
+		// Port already open
+		ports[tabid].postMessage(message);
 	}
 }
