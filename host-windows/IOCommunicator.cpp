206da423c907a29549c0c1dc5f33729f6e9601ff
Rainer Villido <Rainer.Villido@nortal.com>
2015-03-20 13:55:28 +0200

    Added LGPL license header to all source files

--- host-windows/IOCommunicator.cpp
+++ host-windows/IOCommunicator.cpp
@@ -1,12 +1,20 @@
-/* Chrome Linux plugin
-*
-* This software is released under either the GNU Library General Public
-* License (see LICENSE.LGPL).
-*
-* Note that the only valid version of the LGPL license as far as this
-* project is concerned is the original GNU Library General Public License
-* Version 2.1, February 1999
-*/
+/*
+ * Chrome Token Signing Native Host
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 #include "IOCommunicator.h"
 #include "Logger.h"
