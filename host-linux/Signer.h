fa43a47bdec9af80359162f05f730c1a504403c7
Jean-Baptiste Krohn <mail@jbk.io>
2018-01-19 06:37:02 +0000

    Fix sign comparison warning

--- host-linux/Signer.h
+++ host-linux/Signer.h
@@ -210,7 +210,7 @@ private:
             pin->setValidator(new QRegExpValidator(QRegExp(QString("\\d{%1,12}").arg(minPinLen)), pin));
             pin->setMaxLength(12);
             connect(pin, &QLineEdit::textEdited, [=](const QString &text){
-                ok->setEnabled(text.size() >= minPinLen);
+                ok->setEnabled((unsigned long)text.size() >= minPinLen);
             });
 
             layout->addWidget(pin);
